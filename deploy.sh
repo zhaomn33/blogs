bb6a4dc827e74c071c98ca9221cf2001c7943604
zhaomn33 <zhaomn_314@163.com>
2023-01-29 11:23:27 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
-git push -f git@github.com:zhaomn33/blogs.git master:blogs
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:<BranchName>
+git push -f git@github.com:zhaomn33/blogs.git main:blogs
 
 cd -
 
