ab8a8925a58a81bc218beb1cc61fd7a7d9ccefb8
zhaomn33 <zhaomn_314@163.com>
2023-01-29 16:13:27 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -14,6 +14,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhaomn33/zhao-blog.git master:gh-pages
+git push -f git@github.com:zhaomn33/zhao-blog.git main:gh-pages
 
 cd -
+
+# dash deploy.sh
